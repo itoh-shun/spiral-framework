be1413112a40b26351123214637c237c5666d445
APKAVIFL7EWRG4EKB4U4 <ito.shun@pi-pe.co.jp>
2023-10-16 18:20:43 +0900

    生成ロジックを変更

--- Command/Commands/Initialize/UseCases/CreateProjectDefaultFiles.php
+++ Command/Commands/Initialize/UseCases/CreateProjectDefaultFiles.php
@@ -14,11 +14,11 @@ class CreateProjectDefaultFiles {
         $text = View::forge('spiral-framework/Command/Samples/makeAutoload',compact('projectName','dir'));
         file_put_contents("src/{$projectName}/makeAutoload.php", $text);
         
-        $text = View::forge('spiral-framework/Command/Samples/StartUp/web',compact('projectName'));
-        file_put_contents("src/{$projectName}/StartUp/web.php", $text);
+        $text = View::forge('spiral-framework/Command/Samples/routes/web',compact('projectName'));
+        file_put_contents("src/{$projectName}/routes/web.php", $text);
         
-        $text = View::forge('spiral-framework/Command/Samples/StartUp/api',compact('projectName'));
-        file_put_contents("src/{$projectName}/StartUp/api.php", $text);
+        $text = View::forge('spiral-framework/Command/Samples/routes/api',compact('projectName'));
+        file_put_contents("src/{$projectName}/routes/api.php", $text);
 
         $text = View::forge('spiral-framework/Command/Samples/projectApplication',compact('projectName'));
         file_put_contents("src/{$projectName}/{$projectName}Application.php", $text);
@@ -26,11 +26,11 @@ class CreateProjectDefaultFiles {
         $text = View::forge('spiral-framework/Command/Samples/config/app',compact('projectName'));
         file_put_contents("src/{$projectName}/config/app.php", $text);
 
-        $text = View::forge('spiral-framework/Command/Samples/Exceptions/ExceptionHandler',compact('projectName'));
-        file_put_contents("src/{$projectName}/Exceptions/ExceptionHandler.php", $text);
+        $text = View::forge('spiral-framework/Command/Samples/app/Exceptions/ExceptionHandler',compact('projectName'));
+        file_put_contents("src/{$projectName}/app/Exceptions/ExceptionHandler.php", $text);
         
-        $text = View::forge('spiral-framework/Command/Samples/InterfaceAdapters/Controlers/WelcomeController',compact('projectName'));
-        file_put_contents("src/{$projectName}/InterfaceAdapters/Controlers/Web/WelcomeController.php", $text);
+        $text = View::forge('spiral-framework/Command/Samples/app/Http/Controllers/WelcomeController',compact('projectName'));
+        file_put_contents("src/{$projectName}/app/Http/Controllers/Web/WelcomeController.php", $text);
 
         $text = View::forge('spiral-framework/Command/Samples/resources/template/base.blade');
         file_put_contents("src/{$projectName}/resources/template/base.blade.php", $text);
--- Command/Commands/Initialize/UseCases/CreateProjectDirectory.php
+++ Command/Commands/Initialize/UseCases/CreateProjectDirectory.php
@@ -9,20 +9,16 @@ class CreateProjectDirectory {
     public static function execute($inputdata)
     {
         mkdir("src/$inputdata->projectName", 0700, true);
-        mkdir("src/$inputdata->projectName/Application/Api", 0700, true);
-        mkdir("src/$inputdata->projectName/Application/Web", 0700, true);
-        mkdir("src/$inputdata->projectName/Enterprise", 0700, true);
-        mkdir("src/$inputdata->projectName/Exceptions", 0700, true);
-        mkdir("src/$inputdata->projectName/InterfaceAdapters/Controlers/Web", 0700, true);
-        mkdir("src/$inputdata->projectName/InterfaceAdapters/Controlers/Api", 0700, true);
-        mkdir("src/$inputdata->projectName/InterfaceAdapters/GateWays", 0700, true);
-        mkdir("src/$inputdata->projectName/InterfaceAdapters/GateWays/Repository", 0700, true);
-        mkdir("src/$inputdata->projectName/InterfaceAdapters/GateWays/Middleware", 0700, true);
-        mkdir("src/$inputdata->projectName/InterfaceAdapters/Presenters", 0700, true);
+        mkdir("src/$inputdata->projectName/app", 0700, true);
+        mkdir("src/$inputdata->projectName/app/Http/Controllers/Web", 0700, true);
+        mkdir("src/$inputdata->projectName/app/Http/Controllers/Api", 0700, true);
+        mkdir("src/$inputdata->projectName/app/Http/Middleware", 0700, true);
+        mkdir("src/$inputdata->projectName/app/Exceptions", 0700, true);
+        mkdir("src/$inputdata->projectName/app/Providers", 0700, true);
         mkdir("src/$inputdata->projectName/resources/html", 0700, true);
         mkdir("src/$inputdata->projectName/resources/mail", 0700, true);
         mkdir("src/$inputdata->projectName/resources/template", 0700, true);
-        mkdir("src/$inputdata->projectName/StartUp", 0700, true);
+        mkdir("src/$inputdata->projectName/routes", 0700, true);
         mkdir("src/$inputdata->projectName/config", 0700, true);
     }
 }
