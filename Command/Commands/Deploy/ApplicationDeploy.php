3bcc377e63e930ec2ff44a45fa1ee8c6b7cdbcf0
APKAVIFL7EWRG4EKB4U4 <ito.shun@pi-pe.co.jp>
2023-10-30 14:12:16 +0900

    コミット修正

--- Command/Commands/Deploy/ApplicationDeploy.php
+++ Command/Commands/Deploy/ApplicationDeploy.php
@@ -221,11 +221,8 @@ class ApplicationDeploy extends Command
                 $this->rmdir_recursively(".tmp/$environment/src");
             }
         } else {
-            exec("cp -r src/* .tmp/$environment");
-        }
-        
-        if(!$skip){
             exec("cp -r spiral-framework/src/* .tmp/$environment");
+            exec("cp -r src/* .tmp/$environment");
         }
     }
     
